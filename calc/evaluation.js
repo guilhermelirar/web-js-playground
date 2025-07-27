100b1c3b4c9ecdf54f796e18bce22c48c8469f7a
Guilherme <gui.liraribeiro10@gmail.com>
2025-07-27 11:23:15 -0300

    calc: passando no teste, tokenização simples

--- calc/evaluation.js
+++ calc/evaluation.js
@@ -8,6 +8,42 @@ function token(type, value) {
 //      {type:'number', value: '2'}]      ]
 function tokenize(input) {
     let tokens = []
+    let i = 0;
+
+    while (i < input.length) {
+        char = input[i];
+
+        if (/[0-9.]/.test(char)) {
+            let numstr = ""
+            while (i < input.length && /[0-9.]/.test(input[i])) {
+                numstr += input[i++];
+            }
+
+            tokens.push(token("number", numstr))
+            continue;
+        }
+
+        i++;
+        switch (char) {
+            case ('+'): {
+                tokens.push(token('plus', '+'));
+                continue;
+            }
+            case ('-'): {
+                tokens.push(token('minus', '-'));
+                continue;
+            }
+            case ('*'): {
+                tokens.push(token('times', '*'));
+                continue;
+            }
+            case ('/'): {
+                tokens.push(token('div', '/'));
+                continue;
+            }
+        }
+    }
+
     return tokens
 }
 
